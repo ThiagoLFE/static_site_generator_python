e0470590c6294246cb55fd2b3d124d76aeca9f33
thiagofirered <thiago.rib1905@gmail.com>
2026-02-18 22:27:09 -0300

    feat: added tests to guarantee our class working good

---
+++ src/test_leafnode.py
@@ -0,0 +1,17 @@
+import unittest
+
+from leafnode import LeafNode
+
+class testLeafNode(unittest.TestCase):
+
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_without_value_and_tries_to_parse_html(self):
+        with self.assertRaises(ValueError):
+            html_content = LeafNode("p", None, None).to_html()
+    
+
+if __name__ == "__main__":
+    unittest.main()
