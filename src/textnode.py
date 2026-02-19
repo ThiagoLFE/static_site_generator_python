5289f14dde7557c4146a9309c930286d4e9fa25f
thiagofirered <thiago.rib1905@gmail.com>
2026-02-18 23:18:00 -0300

    feat: created helper function to allow us parse a textnode to a html node

--- src/textnode.py
+++ src/textnode.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from leafnode import LeafNode
 
 class TextType(Enum):
     BOLD = "bold"
@@ -19,5 +20,4 @@ class TextNode:
     
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
-    
     
