001401ab91e324ca8a19550c090612cf58467981
thiagofirered <thiago.rib1905@gmail.com>
2026-02-18 23:21:50 -0300

    feat: added a fews tests to make sure all is going good

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,5 +1,4 @@
 import unittest
-from textnode_to_htmlnode import text_node_to_html_node
 from textnode import TextNode, TextType
 
 
@@ -18,10 +17,6 @@ class TestTextNode(unittest.TestCase):
         node1 = TextNode("this is a link text node", TextType.LINK)
         self.assertIsInstance(node1, TextNode)
 
-    def test_text(self):
-        node = TextNode("This is a text node", TextType.TEXT)
-        html_node = text_node_to_html_node(node)
-        self.assertEqual(html_node.tag, None)
-        self.assertEqual(html_node.value, "This is a text node")
+
 if __name__ == "__main__":
     unittest.main()
